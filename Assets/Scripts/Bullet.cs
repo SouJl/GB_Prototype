ecc5c6220557148a2dfc609bfeaac73625e373b7
Akexandr <meln.alex97@gmail.com>
2022-06-18 23:04:09 +0700

    Door Tag
    добавление тега Door и проверки столкновения пули с объектами этого типа

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -27,7 +27,7 @@ public class Bullet : MonoBehaviour
             Destroy(other.gameObject);
             Destroy(gameObject);
         }
-        if (other.gameObject.tag == "Level") Destroy(gameObject);
+        if (other.gameObject.tag == "Level" || other.gameObject.tag == "Door") Destroy(gameObject);
     }
 
 }
